cd444cc9898184e2879574af1c97d84554e663eb
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-14 12:16:14 +0300

    Initial create

---
+++ HTMLPreviewerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTMLPreviewerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
