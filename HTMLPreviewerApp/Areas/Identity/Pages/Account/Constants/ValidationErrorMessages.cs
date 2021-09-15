62e54e9543de68fa2bd61d6ea30d5ed86131b1a1
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-15 12:40:25 +0300

    Implement login logic

---
+++ HTMLPreviewerApp/Areas/Identity/Pages/Account/Constants/ValidationErrorMessages.cs
@@ -0,0 +1,20 @@
+﻿namespace HTMLPreviewerApp.Areas.Identity.Pages.Account.Constants
+{
+    public class ValidationErrorMessages
+    {
+        public const string RequiredEmail = "Valid email is required.";
+        public const string InvalidEmail = "Invalid email";
+        public const string RequiredPassword = "Password is required.";
+        public const string InvalidPasswordContent = "The password must contain a capital letter, a lowercase letter and a number.";
+        public const string InvalidPasswordLength = "Password must be between {2} and {1} characters.";
+        public const string InvalidPasswordConfirmation = "Password and password confirmation do not match.";
+
+        public const string InvalidAttempt = "Invalid login attempt.";
+
+        public const string InvalidPassword = "Invalid password.";
+        public const string RequiredField = "Field is required.";
+
+        public const string AlreadyExistUserWithEmail = "A user with this email already exists.";
+        public const string InvalidChangeEmail = "Грешка в смяната на имейла.";
+    }
+}
--- HTMLPreviewerApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HTMLPreviewerApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,40 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using HTMLPreviewerApp.Data.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HTMLPreviewerApp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+using static HTMLPreviewerApp.Areas.Identity.Pages.Account.Constants.ValidationErrorMessages;
 
 namespace HTMLPreviewerApp.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
-        private readonly ILogger<LoginModel> _logger;
+        private const string RememberMeDisplayName = "Remember Me";
 
-        public LoginModel(SignInManager<User> signInManager, 
-            ILogger<LoginModel> logger,
-            UserManager<User> userManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
-        }
+        private readonly SignInManager<User> _signInManager;
+
+        public LoginModel(SignInManager<User> signInManager)
+            => this._signInManager = signInManager;
 
         [BindProperty]
         public InputModel Input { get; set; }
-
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
         public string ReturnUrl { get; set; }
 
@@ -43,15 +31,15 @@ namespace HTMLPreviewerApp.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = RequiredEmail)]
             [EmailAddress]
             public string Email { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = RequiredPassword)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = RememberMeDisplayName)]
             public bool RememberMe { get; set; }
         }
 
@@ -64,10 +52,7 @@ namespace HTMLPreviewerApp.Areas.Identity.Pages.Account
 
             returnUrl ??= Url.Content("~/");
 
-            // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             ReturnUrl = returnUrl;
         }
@@ -76,35 +61,24 @@ namespace HTMLPreviewerApp.Areas.Identity.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await this._signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
                 }
                 if (result.IsLockedOut)
                 {
-                    _logger.LogWarning("User account locked out.");
                     return RedirectToPage("./Lockout");
                 }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
+
+                ModelState.AddModelError("#", InvalidAttempt);
+
+                return Page();
             }
 
-            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
