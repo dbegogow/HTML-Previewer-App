8594fe83eded08575dc45c9b0f11fd89660c3b57
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-15 10:44:05 +0300

    Implement javascript function for auto rending html

---
+++ HTMLPreviewerApp/wwwroot/js/home/index.js
@@ -0,0 +1,12 @@
+﻿(() => {
+    const resultEl = document.getElementById('result');
+
+    const render = (e) => {
+        const target = e.target;
+
+        resultEl.innerHTML = target.value;
+    };
+
+    document.getElementById('code')
+        .addEventListener('input', render);
+})();
