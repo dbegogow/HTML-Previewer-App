a93e88cdf1144e917d4177837223b68bf5fdaf5e
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-15 17:40:46 +0300

    Implement samples styles

---
+++ HTMLPreviewerApp/Controllers/SamplesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTMLPreviewerApp.Controllers
+{
+    public class SamplesController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
